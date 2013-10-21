9e42dc500faf23393c61a1d5d6a874396234440f
Bhaskar Nidumukalla <bhaskar@denaglobal.com>
2013-10-20 21:36:53 -0700

    ssh functionality

--- lib/vagrant-cssh/command.rb
+++ lib/vagrant-cssh/command.rb
@@ -2,33 +2,60 @@ module VagrantPlugins
   module Cssh
     class Command < Vagrant.plugin(VAGRANT_VERSION, :command)
       def execute
-        cmd, cmd_args = parse_args
-        puts "Hello World!"
-        0
-      end
+        options = {}
 
-      private
-
-      def parse_args
         opts = OptionParser.new do |o|
-          o.banner = 'Usage: vagrant cssh [options] <command>'
-          o.separator ''
+          o.banner = "Usage: vagrant cssh [vm-name] [-c command] [-- extra ssh args]"
 
-          o.on('-h', '--help', 'Print this help') do
-            safe_puts(opts.help)
+          o.separator ""
+
+          o.on("-c", "--command COMMAND", "Execute an SSH command directly.") do |c|
+            options[:command] = c
+          end
+
+          o.on("-p", "--plain", "Plain mode, leaves authentication up to user.") do |p|
+            options[:plain_mode] = p
           end
         end
 
-        argv = split_main_and_subcommand(@argv.dup)
-        exec_args, cmd, cmd_args = argv[0], argv[1], argv[2]
-
-        # show help
-        if !cmd || exec_args.any? { |a| a == '-h' || a == '--help' }
-          safe_puts(opts.help)
-          return nil
+        # Parse out the extra args to send to SSH, which is everything
+        # after the "--"
+        split_index = @argv.index("--")
+        if split_index
+          options[:ssh_args] = @argv.drop(split_index + 1)
+          @argv              = @argv.take(split_index)
         end
 
-        return cmd, cmd_args
+        # Parse the options and return if we don't have any target.
+        argv = parse_options(opts)
+        return if !argv
+
+        # Execute the actual SSH
+        with_target_vms(argv, :single_target => true) do |vm|
+          if options[:command]
+            @logger.debug("Executing single command on remote machine: #{options[:command]}")
+            env = vm.action(:ssh_run, :ssh_run_command => options[:command])
+
+            # Exit with the exit status of the command or a 0 if we didn't
+            # get one.
+            exit_status = env[:ssh_run_exit_status] || 0
+            return exit_status
+          else
+            opts = {
+              :plain_mode => options[:plain_mode],
+              :extra_args => options[:ssh_args]
+            }
+
+            @logger.debug("Invoking `ssh` action on machine")
+            vm.action(:ssh, :ssh_opts => opts)
+
+            # We should never reach this point, since the point of `ssh`
+            # is to exec into the proper SSH shell, but we'll just return
+            # an exit status of 0 anyways.
+            return 0
+          end
+        end
+
       end
     end
   end
